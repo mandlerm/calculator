362698aa4b17548cba8d4fa23e381214ce240b06
mandlerm <amaya.melissa@gmail.com>
2019-06-10 19:48:43 -0500

    skeleton of welcome and calculator components

---
+++ src/Calculator.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Calculator.css';
+
+class Calculator extends React.Component {
+render() {
+    return (
+        <div class="Calculator">
+        Calculate Something
+        </div>
+    );
+}
+}
+
+export default Calculator;
---
+++ src/Welcome.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Welcome.css';
+
+class Welcome extends React.Component {
+    render() {
+      return (
+        <div class="Welcome-header">
+          Welcome to the calculator.
+        </div>
+      );
+    }
+  }
+  
+  
+export default Welcome;
