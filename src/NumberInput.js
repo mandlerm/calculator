092a4ba57fd4eabedd32d724ecc3050fcb5c7c1f
mandlerm <amaya.melissa@gmail.com>
2019-06-11 21:24:43 -0500

    skeleton of NumberInput and Answer components

---
+++ src/NumberInput.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './NumberInput.css';
+
+class NumberInput extends React.Component {
+render() {
+    return (
+        <div class="NumberInput">
+        
+       
+        </div>
+    );
+}
+}
+
+export default NumberInput;
